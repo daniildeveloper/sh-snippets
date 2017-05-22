de47d10f30916f27f61ae506cba710111f962b3c
daniiltserin <daniilborowkow@yandex.ru>
2017-05-22 09:11:44 +0600

    lravel and mysql instll

---
+++ laraveldirs.sh
@@ -0,0 +1,2 @@
+chown -R www-data:www-data /var/www/laravel
+chmod -R 775 /var/www/laravel/storage
--- mysql.sh
+++ mysql.sh
@@ -1,1 +1,5 @@
 # INSTALL
+apt-get -y install mysql-client mysql-server
+
+# make server more secure
+mysql_secure_installation
